fa9998f40199e1f899b9b34c51151612072bc83d
wp360 <2897406527@qq.com>
2017-08-08 00:42:01 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import './index.css';
+import App from './App';
+import rootReducer from './rootReducer';
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(
+    rootReducer
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
+registerServiceWorker();
---
+++ src/rootReducer.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux';
+import movies from './reducers/movies';
+
+export default combineReducers({
+    movies
+});
